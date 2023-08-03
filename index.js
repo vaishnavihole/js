2d6952d1a29687f9cfbccfb5f48a3437a6759a13
vaishnavihole <vaishnavihole1@gmail.com>
2023-08-03 08:39:03 +0530

    SCOPE CHAIN AND LEXICAL ENVIROMENT

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+// HOISTING IN JAVASCRIPT
 // getName();
 // console.log(x);
 
-var x = 7;
+// var x = 7;
 
-function getName() {
-    console.log("Namaste Javascript");
+// function getName() {
+//     console.log("Namaste Javascript");
+// }
+
+
+// FUNCTION IN JAVASCRIPT
+
+// function  a () {
+//    var x = 10;
+//    console.log(x);
+// }
+
+
+// SCOPE CHAIN AND LEXICAL ENVIROMENT
+function a() {
+    console.log(b);
 }
+var b = 10;
+a();
+
+
 
